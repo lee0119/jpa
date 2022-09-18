fb1664cee47cd4860976e28b7fbaf2a58c901268
lee0119 <candogo91@gmail.com>
2022-09-18 21:40:11 +0900

    Study

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -3,6 +3,9 @@ package jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+//2022 09 18 
 @SpringBootApplication
 public class JpashopApplication {
 
